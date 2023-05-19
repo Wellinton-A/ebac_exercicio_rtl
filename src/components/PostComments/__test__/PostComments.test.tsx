3c0ff9c79203b2a9d0852915191612d87f1a986f
Wellinton Alexandre <wellinton1067@gmail.com>
2023-05-18 22:10:43 -0300

    added test for comments

---
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Post from '..';
+import PostComment from '..';
+
+describe('Teste para o componente PostComment', () => {
+    it('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>);
+        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+});
+
+describe('Teste de criacao de comentarios', () => {
+    it('Postagem de primeiro comentario', () => {
+        const { debug } = render(<Post />)
+        const inputComent = screen.getByTestId('comentTextArea')
+        const postButton = screen.getByTestId('post-button')
+
+        fireEvent.change(inputComent, {
+            target: {
+                value: 'Nao gostuei, Asmei!!'
+            }
+        })
+        fireEvent.click(postButton)
+
+        fireEvent.change(inputComent, {
+            target: {
+                value: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            }
+        })
+        fireEvent.click(postButton)
+        expect(screen.getByText('Nao gostuei, Asmei!!')).toBeInTheDocument()
+        expect(screen.getByText('https://www.youtube.com/watch?v=dQw4w9WgXcQ')).toBeInTheDocument()
+    })
+})
